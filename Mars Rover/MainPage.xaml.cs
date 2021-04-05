9ad6ed56247cbc7003e0479726fcaa741f5510e2
Eugene Mulder <eugene@freedomsoftware.co.za>
2021-04-05 15:32:00 +0200

    Commenting

---
+++ Mars Rover/MainPage.xaml.cs
@@ -0,0 +1,227 @@
+﻿using Mars_Rover.Rover;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MUXC = Microsoft.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Mars_Rover
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        RoverCommands[] rovers = new RoverCommands[5];
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void startBTN_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.mainGrid.Visibility == Visibility.Visible)
+            {
+                announce.Text = "Rovers landed successfully in various locations!";
+                this.platGrid.Visibility = Visibility.Visible;
+                this.mainGrid.Visibility = Visibility.Collapsed;
+                int x = Convert.ToInt32(platX.Value);
+                int y = Convert.ToInt32(platY.Value);
+                int nrR = Convert.ToInt32(nrRovers.Value);                              
+                for (int i = 0; i < nrR; i++)
+                {
+                    rovers[i] = new RoverCommands(x, y);
+                }
+                if (nrR == 5)
+                {
+                    r1Out.Text = rovers[0].getInitValues();
+                    r2Out.Text = rovers[1].getInitValues();
+                    r3Out.Text = rovers[2].getInitValues();
+                    r4Out.Text = rovers[3].getInitValues();
+                    r5Out.Text = rovers[4].getInitValues();
+
+                    this.r2Out.Visibility = Visibility.Visible;
+                    this.r2Title2.Visibility = Visibility.Visible;
+                    this.r2Com.Visibility = Visibility.Visible;
+                    this.r2Title.Visibility = Visibility.Visible;
+
+                    this.r3Out.Visibility = Visibility.Visible;
+                    this.r3Title2.Visibility = Visibility.Visible;
+                    this.r3Com.Visibility = Visibility.Visible;
+                    this.r3Title.Visibility = Visibility.Visible;
+
+                    this.r4Out.Visibility = Visibility.Visible;
+                    this.r4Title2.Visibility = Visibility.Visible;
+                    this.r4Com.Visibility = Visibility.Visible;
+                    this.r4Title.Visibility = Visibility.Visible;
+
+                    this.r5Out.Visibility = Visibility.Visible;
+                    this.r5Title2.Visibility = Visibility.Visible;
+                    this.r5Com.Visibility = Visibility.Visible;
+                    this.r5Title.Visibility = Visibility.Visible;
+                }
+                else if (nrR == 4)
+                {
+                    r1Out.Text = rovers[0].getInitValues();
+                    r2Out.Text = rovers[1].getInitValues();
+                    r3Out.Text = rovers[2].getInitValues();
+                    r4Out.Text = rovers[3].getInitValues();
+
+                    this.r2Out.Visibility = Visibility.Visible;
+                    this.r2Title2.Visibility = Visibility.Visible;
+                    this.r2Com.Visibility = Visibility.Visible;
+                    this.r2Title.Visibility = Visibility.Visible;
+
+                    this.r3Out.Visibility = Visibility.Visible;
+                    this.r3Title2.Visibility = Visibility.Visible;
+                    this.r3Com.Visibility = Visibility.Visible;
+                    this.r3Title.Visibility = Visibility.Visible;
+
+                    this.r4Out.Visibility = Visibility.Visible;
+                    this.r4Title2.Visibility = Visibility.Visible;
+                    this.r4Com.Visibility = Visibility.Visible;
+                    this.r4Title.Visibility = Visibility.Visible;
+
+                    this.r5Out.Visibility = Visibility.Collapsed;
+                    this.r5Title2.Visibility = Visibility.Collapsed;
+                    this.r5Com.Visibility = Visibility.Collapsed;
+                    this.r5Title.Visibility = Visibility.Collapsed;
+                }
+                else if (nrR == 3)
+                {
+                    r1Out.Text = rovers[0].getInitValues();
+                    r2Out.Text = rovers[1].getInitValues();
+                    r3Out.Text = rovers[2].getInitValues();
+
+                    this.r2Out.Visibility = Visibility.Visible;
+                    this.r2Title2.Visibility = Visibility.Visible;
+                    this.r2Com.Visibility = Visibility.Visible;
+                    this.r2Title.Visibility = Visibility.Visible;
+
+                    this.r3Out.Visibility = Visibility.Visible;
+                    this.r3Title2.Visibility = Visibility.Visible;
+                    this.r3Com.Visibility = Visibility.Visible;
+                    this.r3Title.Visibility = Visibility.Visible;
+
+                    this.r4Out.Visibility = Visibility.Collapsed;
+                    this.r4Title2.Visibility = Visibility.Collapsed;
+                    this.r4Com.Visibility = Visibility.Collapsed;
+                    this.r4Title.Visibility = Visibility.Collapsed;
+
+                    this.r5Out.Visibility = Visibility.Collapsed;
+                    this.r5Title2.Visibility = Visibility.Collapsed;
+                    this.r5Com.Visibility = Visibility.Collapsed;
+                    this.r5Title.Visibility = Visibility.Collapsed;
+                }
+                else if (nrR == 2)
+                {
+                    r1Out.Text = rovers[0].getInitValues();
+                    r2Out.Text = rovers[1].getInitValues();
+
+                    this.r2Out.Visibility = Visibility.Visible;
+                    this.r2Title2.Visibility = Visibility.Visible;
+                    this.r2Com.Visibility = Visibility.Visible;
+                    this.r2Title.Visibility = Visibility.Visible;
+
+                    this.r3Out.Visibility = Visibility.Collapsed;
+                    this.r3Title2.Visibility = Visibility.Collapsed;
+                    this.r3Com.Visibility = Visibility.Collapsed;
+                    this.r3Title.Visibility = Visibility.Collapsed;
+
+                    this.r4Out.Visibility = Visibility.Collapsed;
+                    this.r4Title2.Visibility = Visibility.Collapsed;
+                    this.r4Com.Visibility = Visibility.Collapsed;
+                    this.r4Title.Visibility = Visibility.Collapsed;
+
+                    this.r5Out.Visibility = Visibility.Collapsed;
+                    this.r5Title2.Visibility = Visibility.Collapsed;
+                    this.r5Com.Visibility = Visibility.Collapsed;
+                    this.r5Title.Visibility = Visibility.Collapsed;
+                }
+                else if (nrR == 1)
+                {
+                    r1Out.Text = rovers[0].getInitValues();
+
+                    this.r2Out.Visibility = Visibility.Collapsed;
+                    this.r2Title2.Visibility = Visibility.Collapsed;
+                    this.r2Com.Visibility = Visibility.Collapsed;
+                    this.r2Title.Visibility = Visibility.Collapsed;
+
+                    this.r3Out.Visibility = Visibility.Collapsed;
+                    this.r3Title2.Visibility = Visibility.Collapsed;
+                    this.r3Com.Visibility = Visibility.Collapsed;
+                    this.r3Title.Visibility = Visibility.Collapsed;
+
+                    this.r4Out.Visibility = Visibility.Collapsed;
+                    this.r4Title2.Visibility = Visibility.Collapsed;
+                    this.r4Com.Visibility = Visibility.Collapsed;
+                    this.r4Title.Visibility = Visibility.Collapsed;
+
+                    this.r5Out.Visibility = Visibility.Collapsed;
+                    this.r5Title2.Visibility = Visibility.Collapsed;
+                    this.r5Com.Visibility = Visibility.Collapsed;
+                    this.r5Title.Visibility = Visibility.Collapsed;
+                }
+            } 
+        }
+
+        private void backBTN_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.platGrid.Visibility == Visibility.Visible)
+            {
+                this.mainGrid.Visibility = Visibility.Visible;
+                this.platGrid.Visibility = Visibility.Collapsed;
+                platGrid.Children.Remove((UIElement)this.FindName("newGrid"));
+            }
+        }
+
+        private void r1BTN_Click(object sender, RoutedEventArgs e)
+        {
+            announce.Text = "Rover commands executed successfully!";
+            int nrR = Convert.ToInt32(nrRovers.Value);
+            int x = Convert.ToInt32(platX.Value);
+            int y = Convert.ToInt32(platY.Value);
+            if (nrR == 5)
+            {
+                r1Out.Text = rovers[0].moveRover(r1Com.Text, r1Out.Text, x, y);
+                r2Out.Text = rovers[1].moveRover(r2Com.Text, r2Out.Text, x, y);
+                r3Out.Text = rovers[2].moveRover(r3Com.Text, r3Out.Text, x, y);
+                r4Out.Text = rovers[3].moveRover(r4Com.Text, r4Out.Text, x, y);
+                r5Out.Text = rovers[4].moveRover(r5Com.Text, r5Out.Text, x, y);
+            }
+            else if (nrR == 4)
+            {
+                r1Out.Text = rovers[0].moveRover(r1Com.Text, r1Out.Text, x, y);
+                r2Out.Text = rovers[1].moveRover(r2Com.Text, r2Out.Text, x, y);
+                r3Out.Text = rovers[2].moveRover(r3Com.Text, r3Out.Text, x, y);
+                r4Out.Text = rovers[3].moveRover(r4Com.Text, r4Out.Text, x, y);
+            }
+            else if (nrR == 3)
+            {
+                r1Out.Text = rovers[0].moveRover(r1Com.Text, r1Out.Text, x, y);
+                r2Out.Text = rovers[1].moveRover(r2Com.Text, r2Out.Text, x, y);
+                r3Out.Text = rovers[2].moveRover(r3Com.Text, r3Out.Text, x, y);
+            }
+            else if (nrR == 2)
+            {
+                r1Out.Text = rovers[0].moveRover(r1Com.Text, r1Out.Text, x, y);
+                r2Out.Text = rovers[1].moveRover(r2Com.Text, r2Out.Text, x, y);
+            }
+            else if (nrR == 1) r1Out.Text = rovers[0].moveRover(r1Com.Text, r1Out.Text, x, y);
+        }
+    }
+}
---
+++ Mars Rover/Rover/RoverCommands.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mars_Rover.Rover
+{
+    class RoverCommands
+    {
+        private string direction = "NWSE";
+        private int initX, initY;
+        private char initD;
+        private Random rnd = new Random();
+
+        public RoverCommands(int platX, int platY)
+        {        
+            initX = rnd.Next(platX);
+            initY = rnd.Next(platY);
+            int dir = rnd.Next(direction.Length);
+            initD = direction[dir];
+        }
+
+        public string getInitValues()
+        {
+            return Convert.ToString(initX) + " " + Convert.ToString(initY) + " " + initD;
+        }
+
+        private string rotationRover(string rot, string curRoverData)
+        {
+            int dir = 0;
+            if (curRoverData == "N") dir = 0;
+            if (curRoverData == "W") dir = 1;
+            if (curRoverData == "S") dir = 2;
+            if (curRoverData == "E") dir = 3;
+            if (rot == "L")
+            {
+                dir++;
+                if (dir > 3) dir = 0;
+            }
+            if (rot == "R")
+            {
+                dir--;
+                if (dir < 0) dir = 3;
+            }
+
+            return Convert.ToString(direction[dir]);
+        }
+
+        public string moveRover(string moveData, string curRoverData, int maxX, int maxY)
+        {
+            moveData = moveData.ToUpper();
+            string breakUp = curRoverData;
+            int firstSpace = breakUp.IndexOf(" ");
+            int curX = Convert.ToInt32(breakUp.Substring(0, firstSpace));
+            breakUp = breakUp.Substring(firstSpace + 1);
+            firstSpace = breakUp.IndexOf(" ");
+            int curY = Convert.ToInt32(breakUp.Substring(0, firstSpace));
+            breakUp = breakUp.Substring(firstSpace + 1);
+            string curD = breakUp;
+            for (int i = 0; i < moveData.Length; i++)
+            {
+                if (moveData[i] == 'M' && curD == "N")
+                {
+                    curY++;
+                    if (curY >= maxY) curY = maxY - 1;
+                }
+                else if (moveData[i] == 'M' && curD == "S")
+                {
+                    curY--;
+                    if (curY < 0) curY = 0;
+                }
+                else if (moveData[i] == 'M' && curD == "W")
+                {
+                    curX--;
+                    if (curX < 0) curX = 0;
+                }
+                else if (moveData[i] == 'M' && curD == "E")
+                {
+                    curX++;
+                    if (curX >= maxX) curX = maxX - 1;
+                }
+                else curD = rotationRover(Convert.ToString(moveData[i]), curD);
+            }
+
+            string finalDestination = Convert.ToString(curX) + " " + Convert.ToString(curY) + " " + curD;
+
+            return finalDestination;
+        }
+    }
+}
